73b3ad3bab5e0455625c13f56f372a385f12bc74
rumman321 <cset-210652@dti.ac>
2024-11-14 22:07:55 +0600

    category btn desgin

---
+++ src/components/layout-components/RightNavBer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RightNavBer = () => {
+    return (
+        <div>
+            right aside added
+            
+        </div>
+    );
+};
+
+export default RightNavBer;
--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import NavBer from '../components/NavBer';
+import LeftNavBer from '../components/layout-components/LeftNavBer';
+import RightNavBer from '../components/layout-components/RightNavBer';
 
 const Home = () => {
     return (
@@ -16,7 +18,15 @@ const Home = () => {
             <nav  className='py-3 w-11/12 mx-auto'>
                 <NavBer></NavBer>
             </nav>
-            <main></main>
+            <main className='py-5 w-11/12 mx-auto grid md:grid-cols-12' >
+            <aside className='left col-span-3'>
+                <LeftNavBer></LeftNavBer>
+            </aside>
+            <section className='col-span-6'>main part</section>
+            <aside className='right col-span-3'>
+                <RightNavBer></RightNavBer>
+            </aside>
+            </main>
         </div>
     );
 };
