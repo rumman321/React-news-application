1c86e83a6f7afbd18c224e5361a370d45a2d03df
rumman321 <cset-210652@dti.ac>
2024-11-14 21:42:38 +0600

    navber desgin complete

---
+++ src/components/NavBer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import userIcon from "../assets/user.png"
+
+const NavBer = () => {
+    return (
+        <div className='flex justify-between items-center'>
+            <div></div>
+            <div className='space-x-5'>
+                <Link to="/">home</Link>
+                <Link to="/about">about</Link>
+                <Link to="career">career</Link>
+            </div>
+            <div className='flex gap-4 items-center'> 
+                <img src={userIcon} alt="" />
+            <button className=' bg-neutral rounded-none text-white btn'>Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBer;
--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
+import NavBer from '../components/NavBer';
 
 const Home = () => {
     return (
-        <div>
+        <div className='font-poppins'>
             <header>
                 <Header></Header>
                 <section className='w-11/12 mx-auto'>
                 <LatestNews></LatestNews>
                 </section>
+                
             </header>
-            <nav></nav>
+            <nav  className='py-3 w-11/12 mx-auto'>
+                <NavBer></NavBer>
+            </nav>
             <main></main>
         </div>
     );
